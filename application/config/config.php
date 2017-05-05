054029bac3cccc8306934806dbcb434416994ce7
Jagmohan Singh <1308js@gmail.com>
2017-05-05 14:28:02 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1:1003/';
-//$config['base_url'] = 'http://52.66.18.75/';
+//$config['base_url'] = 'http://127.0.0.1:1003/';
+$config['base_url'] = 'http://52.66.18.75/';
 
 /*
 |--------------------------------------------------------------------------
